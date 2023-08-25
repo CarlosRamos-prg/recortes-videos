40b98a3c89228c3e54b833147f5a16603226e428
carlos ramos <devcarlosramos@gmail.com>
2023-08-25 06:43:46 +0200

    aqui se muestra la venta aun y nos dice si fue encontra o no encontrada

--- subirvideo.py
+++ subirvideo.py
@@ -1,16 +1,11 @@
 import cv2
 import numpy as np
 import pyautogui
-import time
 
 # Cargar la imagen que deseas buscar
 imagen_a_buscar = cv2.imread('imagen_objetivo.JPG')
 
-contador=0
-
 while True:
-
-    contador=contador+1
     # Capturar una captura de pantalla
     captura = pyautogui.screenshot()
 
@@ -19,26 +14,22 @@ while True:
 
     # Buscar la imagen en la captura de pantalla
     resultado = cv2.matchTemplate(captura_np, imagen_a_buscar, cv2.TM_CCOEFF_NORMED)
-    _, _, _, max_loc = cv2.minMaxLoc(resultado)
+    _, max_val, _, max_loc = cv2.minMaxLoc(resultado)
 
-    # Obtener las coordenadas del punto más cercano de coincidencia
-    altura, ancho, _ = imagen_a_buscar.shape
-    top_left = max_loc
-    bottom_right = (top_left[0] + ancho, top_left[1] + altura)
+    # Establecer un umbral de confianza
+    umbral_confianza = 0.8
 
-    # Dibujar un rectángulo alrededor del objeto encontrado
-    cv2.rectangle(captura_np, top_left, bottom_right, (0, 255, 0), 2)
-
-    # Mostrar la captura con el rectángulo dibujado (ventana más pequeña)
-    ventana_pequeña = cv2.resize(captura_np, (800, 600))
-
-    if(contador==1 or contador==2):
-        # Mostrar la captura con el rectángulo dibujado
+    if max_val >= umbral_confianza:
+        # Imagen encontrada
+        altura, ancho, _ = imagen_a_buscar.shape
+        top_left = max_loc
+        bottom_right = (top_left[0] + ancho, top_left[1] + altura)
+        cv2.rectangle(captura_np, top_left, bottom_right, (0, 255, 0), 2)
+        ventana_pequeña = cv2.resize(captura_np, (800, 600))
         cv2.imshow('Resultado', ventana_pequeña)
-
-    print("Esperando 3 segundos...")
-    time.sleep(3)  # Espera durante 3 segundos
-    print("¡Han pasado 3 segundos!")
+        print("Imagen encontrada.")
+    else:
+        print("Imagen no encontrada.")
 
     # Romper el bucle si se presiona la tecla 'q'
     if cv2.waitKey(1) == ord('q'):
