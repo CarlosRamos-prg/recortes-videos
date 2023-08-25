1f186243e7806800d4027e29348e7a7cb339a9e3
carlos ramos <devcarlosramos@gmail.com>
2023-08-25 07:01:07 +0200

    Guardar mis cambios antes de cambiar de rama

--- subirvideo.py
+++ subirvideo.py
@@ -3,7 +3,7 @@ import numpy as np
 import pyautogui
 
 # Cargar la imagen que deseas buscar
-imagen_a_buscar = cv2.imread('imagen_objetivo.JPG')
+imagen_a_buscar = cv2.imread('Captura.PNG')
 
 while True:
     # Capturar una captura de pantalla
@@ -15,6 +15,12 @@ while True:
     # Buscar la imagen en la captura de pantalla
     resultado = cv2.matchTemplate(captura_np, imagen_a_buscar, cv2.TM_CCOEFF_NORMED)
     _, max_val, _, max_loc = cv2.minMaxLoc(resultado)
+    altura, ancho, _ = imagen_a_buscar.shape
+    top_left = max_loc
+    bottom_right = (top_left[0] + ancho, top_left[1] + altura)
+    cv2.rectangle(captura_np, top_left, bottom_right, (0, 255, 0), 2)
+    ventana_pequeña = cv2.resize(captura_np, (800, 600))
+    cv2.imshow('Resultado', ventana_pequeña)
 
     # Establecer un umbral de confianza
     umbral_confianza = 0.8
